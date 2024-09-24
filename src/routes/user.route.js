532b4f61e5593c389f04e9d2aeced9f47ddee20f
Muhammad Zaid Farid <137697857+zaidFarid2@users.noreply.github.com>
2024-09-25 02:06:03 +0500

    add new routes of user Router

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { updateRefreshToken, userLogin, userLogout, userRegister } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, updateRefreshToken, updateUserAvatar, updateUsercoverImage, updateUserDetails, userLogin, userLogout, userRegister } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWt } from "../middlewares/auth.middleware.js";
 const userRouter = Router()
@@ -25,7 +25,14 @@ userRouter.route("/login").post(userLogin)
 
 userRouter.route("/logout").post(verifyJWt,userLogout)
 userRouter.route("/refresh-token").post(updateRefreshToken)
-    
+userRouter.route("/change-password").post(verifyJWt,changeCurrentPassword)    
+userRouter.route("/get-user").post(verifyJWt,getCurrentUser)
+userRouter.route("/update-account").patch(verifyJWt,updateUserDetails)
+userRouter.route("/avatar").patch(verifyJWt,upload.single("avatar"),updateUserAvatar)
+userRouter.route("/coverImage").patch(verifyJWt,upload.single("coverImage"),updateUsercoverImage)
+userRouter.route("/c/:username").get(verifyJWt,getUserChannelProfile)
+userRouter.route("/history").get(verifyJWt,getWatchHistory)
+
 
 
 export default userRouter
